877c816403fbb82b5ec68b962b4eb9f6196751e3
xyugen <alex.arias.jorym@gmail.com>
2024-02-18 13:50:33 +0800

    add routes

--- src/Layouts/Main.jsx
+++ src/Layouts/Main.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom';
+import { HomePage } from '../Pages/';
 
 const Main = () => {
   return (
     <main>
       <Routes>
+        <Route path='/' element={<HomePage />} />
       </Routes>
     </main>
   )
